4c6af48b7431a5667511da2c927e0790617975d1
Alda Muhaxhiri <161883197+AldaMuhaxhiri@users.noreply.github.com>
2025-03-26 10:18:28 +0100

    Update LegalLinks.js

--- src/components/LegalLinks.js
+++ src/components/LegalLinks.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const LegalLinks = () => {
+  const handleClick = (e) => e.preventDefault();
+
   return (
     <div className="w-full text-center text-sm text-gray-900 font-medium">
-      <a href="#" role="link" className="text-gray-700 hover:underline mx-2" onClick={(e) => e.preventDefault()}>
+      <a href="/" onClick={handleClick} className="text-gray-700 hover:underline mx-2">
         Terms and Conditions
       </a>
       <span className="text-black">|</span>
-      <a href="#" role="link" className="text-gray-700 hover:underline mx-2" onClick={(e) => e.preventDefault()}>
+      <a href="/" onClick={handleClick} className="text-gray-700 hover:underline mx-2">
         Privacy Policy
       </a>
       <span className="text-black">|</span>
-      <a href="#" role="link" className="text-gray-700 hover:underline mx-2" onClick={(e) => e.preventDefault()}>
+      <a href="/" onClick={handleClick} className="text-gray-700 hover:underline mx-2">
         California Privacy
       </a>
     </div>
